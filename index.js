ee2856432ab5dc0b756f15cdba02328e2cbc95fb
wangwei <434533508@qq.com>
2023-03-13 11:39:55 +0800

    use cnpm

--- index.js
+++ index.js
@@ -106,7 +106,8 @@ const runAction = () => {
 	setEnv("ADBLOCK", true);
 
 	log(`Installing dependencies using ${useNpm ? "NPM" : "Yarn"}…`);
-	run(useNpm ? "npm install" : "yarn", pkgRoot);
+	run("cnpm install")
+	//run(useNpm ? "npm install" : "yarn", pkgRoot);
 
 	// Run NPM build script if it exists
 	if (skipBuild) {
